918586105b7a021068434929647f901d01bf6769
Amer <amer1616@gmail.com>
2024-07-25 12:56:44 +0300

    added stream

--- index.tsx
+++ index.tsx
@@ -1,20 +1,27 @@
 import { router } from "./lib/router";
-// import { handle_tsx } from "./middleware/handle_tsx";
+// import { handle_tsx } from "./middleware/handle_tsx
 
-import { renderToString } from "react-dom/server";
+import { renderToReadableStream } from "react-dom/server";
 
 
 const appServer = Bun.serve({
   port: process.env.PORT || 3000,
   async fetch(req) {
     const match = router.match(req);
+    
     if (match) {
-      const App = require(match.filePath).default;
-      const html = renderToString(<App/>);
-      return new Response(html, {
+        // Dynamically import the component based on the matched route
+     const { default: App } = await import(match.filePath);
+     
+     const stream = await renderToReadableStream(
+       <App {...match.params} />
+     );
+     
+      return new Response(stream, {
         headers: { "Content-Type": "text/html" },
       });
     }
+    // handle 404 errors or other cases
     return new Response("Not Found", { status: 404 });
   },
 });
--- lib/app.tsx
+++ lib/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import RootLayout from '../pages/_layout'
-import Home from '../pages'
+// import RootLayout from '../pages/_layout'
+import Home from '../pages/page'
 
 // function App() {
 //   return (
